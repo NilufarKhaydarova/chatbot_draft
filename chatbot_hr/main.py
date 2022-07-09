c9f442b4b50740b2be1600c9a5f3f86e90cc69c2
NILUFAR KHAYDAROVA <38408269+NilufarKhaydarova@users.noreply.github.com>
2022-07-09 10:30:55 +0500

    test

--- chatbot_hr/main.py
+++ chatbot_hr/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa67398627852b1794ef613600e6fe9df9c714362756201faba9679a7b4b42fc
-size 2590
+oid sha256:6a7f4735f3a68e1f0a084330a7740e4ad0b8e4f67d7d21ea718a6e17c87d2821
+size 2596
